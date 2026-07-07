9e4fd9e7d63887f4e4ee3602b50bbe91b9348b3e
fharahap85 <82890658+fharahap85@users.noreply.github.com>
2026-07-07 16:15:15 +0700

    fix: move api to api/index.js for vercel serverless

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 const PORT = process.env.PORT || 3000;
 
